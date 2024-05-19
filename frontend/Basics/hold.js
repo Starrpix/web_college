2113dcdf8d8c38bec8c2cb69eb04cff8dc5e7db3
Starrpix <itzarbin@gmail.com>
2024-05-19 15:03:06 +0545

    Code Changes

--- frontend/Basics/hold.js
+++ frontend/Basics/hold.js
@@ -1,16 +1,29 @@
 
 
+let hoverElement = document.getElementsByClassName("box1")
+
+let backgroundContent = document.getElementsByTagName("body")
+
+let textTxt = document.getElementById("spanId")
 
 
+ hoverElement[0].addEventListener("mouseover",()=>{
+    console.log("hii ")
+    hoverElement[0].textContent="";
+    backgroundContent[0].style.backgroundColor="black"
+     textTxt.style.display="block"
 
-let hold =  document.getElementsByClassName('box1') ="Starry Night"
- hold.addEventListener('mouseover',function(){
-     hold.textContent = 'Starry Night';
- });
+ })
 
- hold.addEventListener('mouseout',function(){
-     hold.textContent = 'Come Here';
-     document.body.style.background.color = '';
- });
+ hoverElement[0].addEventListener('mouseout',function(){
+
+
+  
+         hoverElement[0].innerHTML = "";
+         backgroundContent[0].style.backgroundColor="white"
+         textTxt.style.display="none"
+     });
+
+ 
 
 
